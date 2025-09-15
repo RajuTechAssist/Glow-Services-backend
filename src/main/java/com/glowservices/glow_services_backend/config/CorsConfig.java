0b73ab6ac0017d6fe0a13e3ff43d4c87a810baa3
RajuTechAssist <oooye.raju@gmail.com>
2025-09-15 19:35:03 +0530

    updated CorssConfig to connect to the frontend

--- src/main/java/com/glowservices/glow_services_backend/config/CorsConfig.java
+++ src/main/java/com/glowservices/glow_services_backend/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.glowservices.glow_services_backend.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,10 +16,12 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*"));
+        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("https://glow-services-frontend.vercel.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        
+        corsConfiguration.setExposedHeaders(List.of("Authorization", "Location"));
+        corsConfiguration.setAllowCredentials(true); // only if you plan to use cookies/credentials
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(source);

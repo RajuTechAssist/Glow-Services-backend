ab50f9d96bfe1800893d672e5a83cf02241af8c0
RajuTechAssist <oooye.raju@gmail.com>
2025-11-27 23:08:00 +0530

    Configure AWS S3 for image uploads

--- src/main/java/com/glowservices/glow_services_backend/config/AWSConfig.java
+++ src/main/java/com/glowservices/glow_services_backend/config/AWSConfig.java
@@ -1,5 +1,23 @@
 package com.glowservices.glow_services_backend.config;
 
-public class AWSConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class AWSConfig {
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region)) // Change to your region (e.g., US_EAST_1)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
+    }
 }
--- src/main/java/com/glowservices/glow_services_backend/service/FileUploadService.java
+++ src/main/java/com/glowservices/glow_services_backend/service/FileUploadService.java
@@ -1,5 +1,45 @@
 package com.glowservices.glow_services_backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
 public class FileUploadService {
 
+    @Autowired
+    private S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public String uploadFile(MultipartFile file) {
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+        
+        try {
+            PutObjectRequest putOb = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .contentType(file.getContentType())
+                    // Make public read if your bucket policy allows public access
+                    // .acl("public-read") 
+                    .build();
+
+            s3Client.putObject(putOb, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+            // Return the URL
+            return s3Client.utilities().getUrl(builder -> builder.bucket(bucketName).key(fileName)).toExternalForm();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to upload file to S3", e);
+        }
+    }
 }

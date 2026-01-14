39b1ee0444ef8a17a33b47ec5a18e2b88eaeb605
RajuTechAssist <oooye.raju@gmail.com>
2026-01-15 01:16:16 +0530

    changed api url of booking section

--- src/main/java/com/glowservices/glow_services_backend/controller/BookingController.java
+++ src/main/java/com/glowservices/glow_services_backend/controller/BookingController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping("/api/admin/bookings")
 public class BookingController {
 
     @Autowired
